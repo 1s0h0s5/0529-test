1407d6f51d3e1cc16237bda4c61b104f7cb234fa
littleanimal0917 <51196454+littleanimal0917@users.noreply.github.com>
2019-06-15 21:32:21 +0800

    用matplotlib做圖
    會自動儲存股價圖，每做一個圖就會把之前的圖覆蓋，以免太多檔案

---
+++ new.py
@@ -0,0 +1,64 @@
+def crawl_finance(stock_id):
+    import requests
+    import datetime
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    now = int(datetime.datetime.now().timestamp())+86400
+    url = "https://query1.finance.yahoo.com/v7/finance/download/" + stock_id + ".TW?period1=0&period2=" + str(now) + "&interval=1d&events=history&crumb=hP2rOschxO0"
+    response = requests.post(url)
+
+    with open("stockfile.csv", "w") as f:
+        f.writelines(response.text)
+    
+    import pandas as pd
+    fine_stockdata = pd.read_csv("stockfile.csv", index_col = "Date", parse_dates = ["Date"])
+
+def draw_figure(begin_year,last_year):
+    import pandas as pd
+    yearlist = []
+    for i in range( int(last_year) - int(begin_year) + 1 ):
+        yearlist.append(str(int(begin_year)+i))
+    
+    import csv
+    list_ = []
+    with open("stockfile.csv", "r") as f:
+        tite = f.readline()
+        a = csv.reader( f )
+        for aline in a:
+            if aline[0][:4] in yearlist:
+                list_.append( aline )
+
+    with open("chose_period.csv","w") as c:
+        c.write(tite)
+        for element in list_:
+            for i in range(len(element)):
+                if i == len(element) - 1:
+                    c.write(element[i])
+                else:
+                    c.write(element[i]+",")     
+            c.write("\n")
+
+    with open("chose_period.csv","r") as d:
+        e = pd.read_csv("chose_period.csv", index_col = "Date", parse_dates = ["Date"])
+        import matplotlib.pyplot as plt
+        import numpy as np
+        import os, sys
+        from PIL import Image, ImageTk
+        plt.xlabel('Date')
+        plt.ylabel('Stock Price')
+        fig = e.Close.plot()
+        stock_time_price = 'stock.png'
+        if os.path.isfile(stock_time_price):
+            os.remove(stock_time_price)
+            plt.savefig(stock_time_price)
+        else:
+            plt.savefig(stock_time_price)
+        
+    
+    
+stock_id = input()
+wanted_period = input()
+begin_year = wanted_period.split(",")[0]
+last_year = wanted_period.split(",")[1]
+crawl_finance(stock_id)
+draw_figure(begin_year,last_year)
